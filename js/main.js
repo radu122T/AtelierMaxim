1d316551e30c424325b9c245031345f0770aeff2
radu122T <radu.cudalb@yahoo.ro>
2022-05-07 00:44:54 +0300

    05.07.2022

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-document.addEventListener('click', e => {
+
+//drop down menu
+document.addEventListener('mouseover', e => {
     const isDropdownButton = e.target.matches('[data-dropdown-button]')
     if (!isDropdownButton && e.target.closest('[data-dropdown]') !=null) return
 
@@ -13,3 +15,58 @@ document.addEventListener('click', e => {
         dropdown.classList.remove('active')
     })
 })
+
+
+//Carousel
+const buttons = document.querySelectorAll("[data-carousel-button]")
+console.log(buttons)
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]")
+
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) 
+        newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) 
+        newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
+
+
+const thirdSection =  document.getElementsByClassName('thirdSection')[0]
+
+const buttonsSecond = document.querySelectorAll('[data-second]')
+
+buttonsSecond.forEach(button => {
+    button.addEventListener('click', () => {
+        const offs = button.dataset.second
+        if( offs === 'noutati') { 
+            Array.prototype.forEach.call(thirdSection.children, child => {
+                if (child.classList.contains('noutatiProduse')){
+                    child.classList.remove('noDisplay') 
+                    child.classList.add('display')}
+                else {
+                    child.classList.add('noDisplay')
+                    child.classList.remove('display')}
+        })
+    }
+        else {
+            Array.prototype.forEach.call(thirdSection.children, child => {
+                if (child.classList.contains('celeMaiVanduteProduse')){
+                    child.classList.remove('noDisplay') 
+                    child.classList.add('display')}
+                else {
+                    child.classList.add('noDisplay')
+                    child.classList.remove('display')}
+        })}
+
+    })
+})
+
